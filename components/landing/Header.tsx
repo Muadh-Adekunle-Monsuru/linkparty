37cbfae20a7e1114df9a734e0dd1e5205bd6a514
Riskgratis Technologies Limited <muadh117a@gmail.com>
2026-07-04 11:57:17 +0100

    hiding header

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -5,7 +5,7 @@ import { Menu } from "lucide-react"
 
 export default function Header() {
   return (
-    <header className="flex w-full items-center justify-center rounded-3xl border-8 border-black bg-white px-6 py-4 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] md:px-10 md:py-6 md:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] dark:border-white dark:bg-black dark:shadow-[8px_8px_0px_0px_rgba(255,255,255,1)] md:dark:shadow-[12px_12px_0px_0px_rgba(255,255,255,1)]">
+    <header className="flex w-full items-center justify-center rounded-3xl border-8 border-black bg-white px-6 py-4 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] md:hidden md:px-10 md:py-6 md:shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] dark:border-white dark:bg-black dark:shadow-[8px_8px_0px_0px_rgba(255,255,255,1)] md:dark:shadow-[12px_12px_0px_0px_rgba(255,255,255,1)]">
       <Link href="/" className="flex items-center gap-3">
         <Image
           src="/logo.svg"
